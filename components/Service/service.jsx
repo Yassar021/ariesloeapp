fedd5699b99bfc926f9c071b2c0d5fb7725f71ca
Yassar021 <yassar.tkj114@gmail.com>
2022-06-09 21:21:57 +0700

    doneAllSize

--- components/Service/service.jsx
+++ components/Service/service.jsx
@@ -1,15 +1,15 @@
-import { Box, Button, Flex, HStack, Link, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Link, SimpleGrid, Stack, Text } from "@chakra-ui/react";
 
 export default function Service() {
     return (
-        <Box pb='200px' bgColor='#221048' px='60px' alignItems='center' borderRadius={'30px'} textAlign={'center'}>
+        <Box pb='200px' bgColor='#221048' px={{base:'0px',md:'60px'}} alignItems='center' textAlign={'center'}>
             <Flex direction={'column'}  alignItems='center' textAlign={'center'}>
                 <Text mt='90px' fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}> Get Started</Text>
-                <Text mt='13px' fontSize={'36px'} fontWeight={'700'} color='#fff' letterSpacing={'0.1em'}>What do you need? Choose a <br/> service that can help you</Text>
+                <Text mt='13px' fontSize={'36px'} fontWeight={'700'} color='#fff' letterSpacing={'0.1em'}>What do you need? Choose a service that can help you</Text>
 
                 <SimpleGrid columns={{md:1,lg:2,xl:3,'2xl':3}} spacing='20px' mt='70px'>
-                    <Box w='100%' h='474px' bgColor={'#3F2379'} py='58px' px='47px' borderRadius={'10px'}>
-                        <HStack spacing='48px'>
+                    <Box w={{base:'100%',md:'100%'}} h={{base:'562px',md:'474px'}} bgColor={'#3F2379'} py={'58px'} px={{base:'37px',md:'47px'}} borderRadius={'10px'}>
+                        <Stack direction={{base:'column',md:'row',lg:'row',xl:'row','2xl':'row'}} spacing='48px'>
                             <Box mb='auto'>
                                 <Text fontSize={'16px'} fontWeight='700' color='#fff'>
                                     UI Design
@@ -19,7 +19,7 @@ export default function Service() {
                                 <Text fontSize={'12px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Starting from</Text>
                                 <Text fontSize={'46px'} fontWeight='700' color='#fff'>1200$</Text>
                             </Box>
-                        </HStack>
+                        </Stack>
                         <Box mt='37px' borderTop={'2px solid rgba(255, 255, 255, 0.05)'} />
                         <Box mt='37px' mb='50px'>   
                             <Text fontSize={'16px'} fontWeight='500' color='#fff'>
@@ -55,8 +55,8 @@ export default function Service() {
                             </Button>
                         </Link>
                     </Box>
-                    <Box w='100%' h='474px' bgColor={'#233679'} py='58px' px='47px'  borderRadius={'10px'}>
-                            <HStack spacing='48px'>
+                    <Box w={{base:'100%',md:'100%'}} h={{base:'562px',md:'474px'}} bgColor={'#233679'} py={'58px'} px={{base:'37px',md:'47px'}}  borderRadius={'10px'}>
+                            <Stack direction={{base:'column',md:'row',lg:'row',xl:'row','2xl':'row'}} spacing='48px'>
                                 <Box mb='auto'>
                                     <Text fontSize={'16px'} fontWeight='700' color='#fff'>
                                         Development
@@ -66,7 +66,7 @@ export default function Service() {
                                     <Text fontSize={'12px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Starting from</Text>
                                     <Text fontSize={'46px'} fontWeight='700' color='#fff'>5000$</Text>
                                 </Box>
-                            </HStack>
+                            </Stack>
                             <Box mt='37px' borderTop={'2px solid rgba(255, 255, 255, 0.05)'} />
                             <Box mt='37px' mb='50px'>   
                                 <Text fontSize={'16px'} fontWeight='500' color='#fff'>
@@ -101,9 +101,9 @@ export default function Service() {
                                     Detail
                                     </Button>
                             </Link>
-                        </Box>
-                    <Box ml={{base:'0px',md:'0px',lg:'192px',xl:'0px','2xl':'0px'}} w='100%' h='474px' bgColor={'#792366'} py='58px' px='47px'  borderRadius={'10px'}>
-                        <HStack spacing='48px'>
+                    </Box>
+                    <Box ml={{base:'0px',md:'0px',lg:'192px',xl:'0px','2xl':'0px'}} w={{base:'100%',md:'100%'}} h={{base:'562px',md:'474px'}} bgColor={'#792366'} py={'58px'} px={{base:'37px',md:'47px'}}  borderRadius={'10px'}>
+                            <Stack  direction={{base:'column',md:'row',lg:'row',xl:'row','2xl':'row'}} spacing='48px'>
                                 <Box mb='auto'>
                                     <Text fontSize={'16px'} fontWeight='700' color='#fff'>
                                         Maintenence
@@ -113,7 +113,7 @@ export default function Service() {
                                     <Text fontSize={'12px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Starting from</Text>
                                     <Text fontSize={'46px'} fontWeight='700' color='#fff'>3000$</Text>
                                 </Box>
-                            </HStack>
+                            </Stack>
                             <Box mt='37px' borderTop={'2px solid rgba(255, 255, 255, 0.05)'} />
                             <Box mt='37px' mb='50px'>   
                                 <Text fontSize={'16px'} fontWeight='500' color='#fff'>
