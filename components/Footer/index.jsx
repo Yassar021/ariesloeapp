9a3264d1ce296eee632b2ca4634ac5d27209d2be
Yassar021 <yassar.tkj114@gmail.com>
2023-02-06 15:24:15 +0700

    update year

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -59,7 +59,7 @@ export default function Footer() {
                             <Text letterSpacing={'0.1em'} fontSize={'24px'} fontWeight={'700'} color='#fff'>Aganta</Text>
                         </HStack>
                     </Link>
-                    <Text mt='21px' mb='10px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)'>Copyright © 2022</Text>
+                    <Text mt='21px' mb='10px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)'>Copyright © 2023</Text>
                     {/* <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Design By Collosal LLC</Text> */}
                 </Box>
                 <Box mb='auto'>
