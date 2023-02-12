e91b1b114bce767429c8de4c821095b2e12f8622
Yassar021 <yassar.tkj114@gmail.com>
2023-02-13 04:10:14 +0700

    deleting import not use

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,11 +1,11 @@
-import { Box, Button, Container, Divider, Flex, HStack, Image, Stack, Text } from "@chakra-ui/react";
+import { Box, Container, Divider, Flex, HStack, Image, Text } from "@chakra-ui/react";
 import Link from "next/link";
 
 export default function Footer() {
-    return ( 
+    return (
         <>
-        <Container maxW='8xl'>
-            {/* <Box py={{base:'22px',xl:'46px'}} h={{base:'440px',md:'284px',lg:'190px',xl:'190px','2xl':'190px'}} w='100%' borderTop={'2px solid rgba(255, 255, 255, 0.1)'} borderBottom={'2px solid rgba(255, 255, 255, 0.1)'}>
+            <Container maxW='8xl'>
+                {/* <Box py={{base:'22px',xl:'46px'}} h={{base:'440px',md:'284px',lg:'190px',xl:'190px','2xl':'190px'}} w='100%' borderTop={'2px solid rgba(255, 255, 255, 0.1)'} borderBottom={'2px solid rgba(255, 255, 255, 0.1)'}>
                 <Flex direction={{base:'column',md:'column',lg:'row',xl:'row','2xl':'row'}} alignItems='center' textAlign={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'} } justifyContent='space-between'>
                     <Text fontSize={'30px'} fontWeight={'700'} color='#fff'>We have prepared everything, its time for you to tell the problem</Text>
                     <Stack direction={{base:'column',md:'row',lg:'row',xl:'row','2xl':'row'}}  spacing='23px' mt={{base:'40px',md:'40px',lg:'0px',xl:'0px','2xl':'0px'}} >
@@ -50,86 +50,86 @@ export default function Footer() {
                     </Stack>
                 </Flex>
             </Box> */}
-            <Divider />
-            <Flex pb='65px' direction={{base:'column',md:'row',lg:'row',xl:'row','2xl':'row'}} gap={{base:'40px',md:'0px',}} justifyContent='space-between' mt='105px' alignItems={{base:'center',md:'left',lg:'left',xl:'left','2xl':'left'}} textAlign={{base:'center',md:'left',lg:'left',xl:'left','2xl':'left'}}>
-                <Box mb='auto'>
-                    <Link href='/' _hover={{TextDecoder:'none'}}>
-                        <HStack spacing='12px'>
-                            <Image src='/aganta2.png' w='42px' h='42px' alt='logo' />
-                            <Text letterSpacing={'0.1em'} fontSize={'24px'} fontWeight={'700'} color='#fff'>Aganta</Text>
-                        </HStack>
-                    </Link>
-                    <Text mt='21px' mb='10px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)'>Copyright © 2023</Text>
-                    {/* <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Design By Collosal LLC</Text> */}
-                </Box>
-                <Box mb='auto'>
-                    <Text fontSize={'14px'} fontWeight='500' color='#fff'>Services</Text>
-                    <Link href='/'>
-                        <Text mt='20px' mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>UI / UX Design</Text>
-                    </Link>
-                    <Link href='/'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Web Development</Text>
-                    </Link>
-                    <Link href='/'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Mobile Development</Text>
-                    </Link>
-                    <Link href='/'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Back End / Custom API</Text>
-                    </Link>
-                    <Link href='/'>
-                        <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Developer as Service</Text>
-                    </Link>
-                    <Link href='/'>
-                        <Text mt='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Support and Maintenance</Text>
-                    </Link>
-                </Box>
-                <Box mb='auto'>
-                    <Text fontSize={'14px'} fontWeight='500' color='#fff'>Company</Text>
-                    <Link href='/comingSoon'>
-                        <Text mt='20px' mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>About</Text>
-                    </Link>
-                    <Link href={'/comingSoon'}>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Contact</Text>
-                    </Link>
-                    {/* <Link href='/comingSoon'>
+                <Divider />
+                <Flex pb='65px' direction={{ base: 'column', md: 'row', lg: 'row', xl: 'row', '2xl': 'row' }} gap={{ base: '40px', md: '0px', }} justifyContent='space-between' mt='105px' alignItems={{ base: 'center', md: 'left', lg: 'left', xl: 'left', '2xl': 'left' }} textAlign={{ base: 'center', md: 'left', lg: 'left', xl: 'left', '2xl': 'left' }}>
+                    <Box mb='auto'>
+                        <Link href='/' _hover={{ TextDecoder: 'none' }}>
+                            <HStack spacing='12px'>
+                                <Image src='/aganta2.png' w='42px' h='42px' alt='logo' />
+                                <Text letterSpacing={'0.1em'} fontSize={'24px'} fontWeight={'700'} color='#fff'>Aganta</Text>
+                            </HStack>
+                        </Link>
+                        <Text mt='21px' mb='10px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)'>Copyright © 2023</Text>
+                        {/* <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Design By Collosal LLC</Text> */}
+                    </Box>
+                    <Box mb='auto'>
+                        <Text fontSize={'14px'} fontWeight='500' color='#fff'>Services</Text>
+                        <Link href='/'>
+                            <Text mt='20px' mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>UI / UX Design</Text>
+                        </Link>
+                        <Link href='/'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Web Development</Text>
+                        </Link>
+                        <Link href='/'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Mobile Development</Text>
+                        </Link>
+                        <Link href='/'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Back End / Custom API</Text>
+                        </Link>
+                        <Link href='/'>
+                            <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Developer as Service</Text>
+                        </Link>
+                        <Link href='/'>
+                            <Text mt='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Support and Maintenance</Text>
+                        </Link>
+                    </Box>
+                    <Box mb='auto'>
+                        <Text fontSize={'14px'} fontWeight='500' color='#fff'>Company</Text>
+                        <Link href='/comingSoon'>
+                            <Text mt='20px' mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>About</Text>
+                        </Link>
+                        <Link href={'/comingSoon'}>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Contact</Text>
+                        </Link>
+                        {/* <Link href='/comingSoon'>
                         <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Send Quote</Text>
                     </Link> */}
-                    {/* <Link href='/comingSoon'>
+                        {/* <Link href='/comingSoon'>
                         <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Privacy Policy</Text>
                     </Link> */}
-                    <Link href='/comingSoon'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Term of Service</Text>
-                    </Link>
-                    {/* <Link href='/comingSoon'>
+                        <Link href='/comingSoon'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Term of Service</Text>
+                        </Link>
+                        {/* <Link href='/comingSoon'>
                         <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Jobs</Text>
                     </Link> */}
-                </Box>
-                <Box mb='auto'>
-                    <Text fontSize={'14px'} fontWeight='500' color='#fff'>Resource</Text>
-                    <Link href='/comingSoon'>
-                        <Text mt='20px' mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Support</Text>
-                    </Link>
-                    <Link href='/comingSoon'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Documentation</Text>
-                    </Link>
-                    <Link href='/comingSoon'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>UI Design</Text>
-                    </Link>
-                    <Link href='/comingSoon'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>License</Text>    
-                    </Link>
-                    <Link href='/comingSoon'>
-                        <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Site Map</Text>   
-                    </Link>
-                    <Link href='/comingSoon'>
-                        <Text  mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Blog</Text>
-                    </Link>
-                    <Link href='/comingSoon'>
-                        <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>FAQ</Text>
-                    </Link>
-                </Box>
-            </Flex>
-        </Container>
+                    </Box>
+                    <Box mb='auto'>
+                        <Text fontSize={'14px'} fontWeight='500' color='#fff'>Resource</Text>
+                        <Link href='/comingSoon'>
+                            <Text mt='20px' mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Support</Text>
+                        </Link>
+                        <Link href='/comingSoon'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Documentation</Text>
+                        </Link>
+                        <Link href='/comingSoon'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>UI Design</Text>
+                        </Link>
+                        <Link href='/comingSoon'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>License</Text>
+                        </Link>
+                        <Link href='/comingSoon'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Site Map</Text>
+                        </Link>
+                        <Link href='/comingSoon'>
+                            <Text mb='10px' fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>Blog</Text>
+                        </Link>
+                        <Link href='/comingSoon'>
+                            <Text fontSize={'16px'} fontWeight='400' color='rgba(255, 255, 255, 0.6)'>FAQ</Text>
+                        </Link>
+                    </Box>
+                </Flex>
+            </Container>
         </>
     )
 }
--- components/HowWeWork/index.jsx
+++ components/HowWeWork/index.jsx
@@ -1,9 +1,9 @@
-import { Box, Flex, Text, Link, Stack, Image, Container} from "@chakra-ui/react"
+import { Box, Flex, Text, Stack, Image, Container } from "@chakra-ui/react"
 
 const HowWeWork = () => {
-    return ( 
+    return (
         <Box>
-            <Flex  mt='101px' direction={{base:'column',md:'row',lg:'row',xl:'row','2xl':'row'}} justifyContent='center' textAlign={{base:'center',md:'left',lg:'left',xl:'left','2xl':'left'}}>
+            <Flex mt='101px' direction={{ base: 'column', md: 'row', lg: 'row', xl: 'row', '2xl': 'row' }} justifyContent='center' textAlign={{ base: 'center', md: 'left', lg: 'left', xl: 'left', '2xl': 'left' }}>
                 <Box my='auto' textAlign={'center'}>
                     <Text fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}> HOW WE WORK</Text>
                     <Text mt='13px' fontSize={'36px'} fontWeight='700' color='#fff' lineHeight='54px'>We have a workflow that allows the job to be delivered well</Text>
@@ -11,32 +11,32 @@ const HowWeWork = () => {
             </Flex>
             <Box mt='90px' borderTop={'2px solid rgba(255, 255, 255, 0.1)'} />
             <Container maxW='8xl'>
-                <Stack direction={{base:'column-reverse',md:'column-reverse',lg:'row',xl:'row','2xl':'row'}} alignItems={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}} spacing='66px' mt='124px'>
-                        <Image src='/step1.png' w='411px' h='408px' alt=''  textAlign={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}} />
-                        <Box  w={{base:'auto',md:'100%', lg:'100%',xl:'100%','2xl':'100%'}} my='auto' textAlign={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}}>
-                            <Text fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}>STEP 1</Text> 
-                            <Text mt='4px' fontSize={'30px'} fontWeight='700' color='#fff' lineHeight={'48px'}>Lets talk about your company problems first</Text>    
-                            <Text mt='28px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)' lineHeight={'32px'}>After submitting the quote form, we will review the data, then we will contact you. You can discuss with our team regarding your problem and find a solution to that problem.
-                                <br /><br />In this step, you will discuss what application to build.</Text>
-                        </Box>
+                <Stack direction={{ base: 'column-reverse', md: 'column-reverse', lg: 'row', xl: 'row', '2xl': 'row' }} alignItems={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }} spacing='66px' mt='124px'>
+                    <Image src='/step1.png' w='411px' h='408px' alt='' textAlign={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }} />
+                    <Box w={{ base: 'auto', md: '100%', lg: '100%', xl: '100%', '2xl': '100%' }} my='auto' textAlign={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }}>
+                        <Text fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}>STEP 1</Text>
+                        <Text mt='4px' fontSize={'30px'} fontWeight='700' color='#fff' lineHeight={'48px'}>Lets talk about your company problems first</Text>
+                        <Text mt='28px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)' lineHeight={'32px'}>After submitting the quote form, we will review the data, then we will contact you. You can discuss with our team regarding your problem and find a solution to that problem.
+                            <br /><br />In this step, you will discuss what application to build.</Text>
+                    </Box>
                 </Stack>
-                <Stack direction={{base:'column',md:'column',lg:'row',xl:'row','2xl':'row'}}  alignItems={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}} spacing='66px' my='166px'>
-                        <Box  w={{base:'auto',md:'100%', lg:'100%',xl:'100%','2xl':'100%'}} my='auto' textAlign={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}}>
-                            <Text fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}>STEP 2</Text> 
-                            <Text mt='4px' fontSize={'30px'} fontWeight='700' color='#fff' lineHeight={'48px'}>Doing planning, design and development until everything is finished</Text>    
-                            <Text mt='28px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)' lineHeight={'32px'}>When everything is agreed upon, our team will make plans related to the application that will be created. Starting from analysis, design, to development.
-                                <br /><br />In this step, the application is 100% complete.</Text>
-                        </Box>
-                        <Image src='/step2.png' w='411px' h='408px' alt='' textAlign={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}} />
+                <Stack direction={{ base: 'column', md: 'column', lg: 'row', xl: 'row', '2xl': 'row' }} alignItems={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }} spacing='66px' my='166px'>
+                    <Box w={{ base: 'auto', md: '100%', lg: '100%', xl: '100%', '2xl': '100%' }} my='auto' textAlign={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }}>
+                        <Text fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}>STEP 2</Text>
+                        <Text mt='4px' fontSize={'30px'} fontWeight='700' color='#fff' lineHeight={'48px'}>Doing planning, design and development until everything is finished</Text>
+                        <Text mt='28px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)' lineHeight={'32px'}>When everything is agreed upon, our team will make plans related to the application that will be created. Starting from analysis, design, to development.
+                            <br /><br />In this step, the application is 100% complete.</Text>
+                    </Box>
+                    <Image src='/step2.png' w='411px' h='408px' alt='' textAlign={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }} />
                 </Stack>
-                <Stack direction={{base:'column-reverse',md:'column-reverse',lg:'row',xl:'row','2xl':'row'}} alignItems={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}} spacing='66px' mb='166px'>
-                        <Image src='/step1.png' w='411px' h='408px' alt=''  textAlign={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}} />
-                        <Box  w={{base:'auto',md:'100%', lg:'100%',xl:'100%','2xl':'100%'}} my='auto' textAlign={{base:'center',md:'center',lg:'left',xl:'left','2xl':'left'}}>
-                            <Text fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}>STEP 3</Text> 
-                            <Text mt='4px' fontSize={'30px'} fontWeight='700' color='#fff' lineHeight={'48px'}>The project is complete and we ship all the project assets, and access to the server</Text>    
-                            <Text mt='28px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)' lineHeight={'32px'}>We will be responsible for delivering all the project assets to you, dont worry. These assets include, design files, source code, server access, and so on.
-                                <br /><br />In this step, everything is done and the contract ends.</Text>
-                        </Box>
+                <Stack direction={{ base: 'column-reverse', md: 'column-reverse', lg: 'row', xl: 'row', '2xl': 'row' }} alignItems={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }} spacing='66px' mb='166px'>
+                    <Image src='/step1.png' w='411px' h='408px' alt='' textAlign={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }} />
+                    <Box w={{ base: 'auto', md: '100%', lg: '100%', xl: '100%', '2xl': '100%' }} my='auto' textAlign={{ base: 'center', md: 'center', lg: 'left', xl: 'left', '2xl': 'left' }}>
+                        <Text fontSize={'16px'} fontWeight={'600'} color='#16FCD2' letterSpacing={'0.1em'}>STEP 3</Text>
+                        <Text mt='4px' fontSize={'30px'} fontWeight='700' color='#fff' lineHeight={'48px'}>The project is complete and we ship all the project assets, and access to the server</Text>
+                        <Text mt='28px' fontSize={'16px'} fontWeight='500' color='rgba(255, 255, 255, 0.6)' lineHeight={'32px'}>We will be responsible for delivering all the project assets to you, dont worry. These assets include, design files, source code, server access, and so on.
+                            <br /><br />In this step, everything is done and the contract ends.</Text>
+                    </Box>
                 </Stack>
             </Container>
         </Box>
